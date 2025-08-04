1c9ebffb269da8387989fabd7f42ac50e98423de
Luan <luan.souza.la@gmail.com>
2025-08-04 14:43:31 -0300

    Remover Softwares

---
+++ codigos/Remove_Software.ps1
@@ -0,0 +1,16 @@
+﻿#variavel
+$softw = $null
+
+#lista todos os softwares instalado
+winget list
+
+pause
+
+#remove o software escolhido
+$softw = read-host "`n`nDigite o nome do software a remover"
+cls
+
+write-host "Software a ser removido é o $softw"
+pause
+ 
+winget uninstall --name "$softw"
